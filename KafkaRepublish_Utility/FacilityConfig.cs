6c2b221cc0ecf54ac6ac5919caeda2914f3622bd
Maruti Bhandari <maruti.bhandari@pb.com>
2023-09-27 15:29:23 -0500

    Enabling the app to select facility based on the input parameter.

---
+++ KafkaRepublish_Utility/FacilityConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafkaRepublish_Utility
+{
+    public class InductionFacility
+    {
+        public string FacilityName { get; set; }
+        public string DataSource { get; set; }
+    }
+
+    public class FacilityConfig
+    {
+        public List<InductionFacility> InductionFacility { get; set; }
+    }
+}
